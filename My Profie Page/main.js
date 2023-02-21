ff2f904e11830fe76f398dc27949f90888824215
johndavedosn <112868391+johndavedosn@users.noreply.github.com>
2023-02-21 17:21:31 +0100

    Add files via upload

---
+++ My Profie Page/main.js
@@ -0,0 +1,11 @@
+var rb = document.getElementById("rb");
+function Redirect(url) {
+    window.location.href = url 
+}
+rb.addEventListener("click", () => {
+    Redirect("https://github.com/johndavedosn/Python-Packages/tree/main/Python-Based-Language")
+})
+var rb2 = document.getElementById("rb2")
+rb2.addEventListener("click", () => {
+    Redirect("https://github.com/johndavedosn/HTML-CSS-JS/tree/main/Sign%20In%20Form")
+})
